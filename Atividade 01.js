ed22be81c53c65c41e608be088b655e41b6d4638
saralorranne <sara.l.lima@aluno.senai.br>
2024-01-29 17:36:42 -0300

    Ativ 01.js

---
+++ Atividade 01.js
@@ -0,0 +1,10 @@
+let num=prompt ("Informe um numero");
+let contador =1;
+while (contador <= 10){
+let result = num*contador;
+
+contador=contador+1;
+
+console.log(num+"x"+contador+"="+result);
+contador=contador+1;
+}
