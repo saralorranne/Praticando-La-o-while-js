2eecb70ab87e2bb73dd9578066ff41891c66d766
saralorranne <sara.l.lima@aluno.senai.br>
2024-01-30 16:08:20 -0300

    Ativ 02.js

---
+++ Atividade 02.js
@@ -0,0 +1,12 @@
+let soma = 0 ;
+while(true){
+let num =prompt('informe um numero inteiro e positivoou um numero negativo para encerrar:');
+if(num<0){
+break;
+}
+
+}
+  if (num % 2=== 0){
+  soma +=num;
+  }
+  console.lof('A soma do numero é par:${soma}');
